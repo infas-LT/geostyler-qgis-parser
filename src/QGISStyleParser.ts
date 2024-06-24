2b9af1bf27b5539b3b8246a0e71e2ff7cc0d48f3
Heber, Markus <heber@geomarketing.de>
2024-06-24 10:36:14 +0200

    Removed Change which was committed in this branch unintended

--- src/QGISStyleParser.ts
+++ src/QGISStyleParser.ts
@@ -308,35 +308,7 @@ export class QGISStyleParser implements StyleParser {
           ...symbolizers,
           ...labels
         ]
-      };
-
-    // Additionally, deliver rules for texts
-    if (labelMap && Array.isArray(qmlLabeling?.rules) && qmlLabeling.rules.length>0) {        
-      if (Array.isArray(qmlLabeling.rules[0].rule) && qmlRules.length > 0) {
-        qmlLabeling.rules[0].rule.forEach((qmlLabelRule: QmlRule, index: number) => {
-          const labelfilter: Filter | undefined = this.getFilterFromQmlRule(qmlLabelRule);
-          const labelScaleDenominator: ScaleDenominator | undefined = this.getScaleDenominatorFromRule(qmlLabelRule);
-          const labelName = qmlLabelRule["description"] || qmlLabelRule.$.filter;
-          const labelSymbolizerId: string = qmlLabelRule.$.filter?.toString() || "a";            
-          let labelRule: Rule = <Rule> {          
-          };
-          if (labelName) {
-            labelRule.name = labelName;
-          }
-          if (labelfilter) {
-            labelRule.filter = labelfilter;
-          }
-          if (labelScaleDenominator) {
-            labelRule.scaleDenominator = labelScaleDenominator;
-          }
-          if (Object.keys(labelMap).length > 0 && labelMap[labelSymbolizerId]) {
-            labelRule.symbolizers = labelMap[labelSymbolizerId];
-          }
-          rules.push(labelRule);
-        });
-    }
-
-  }
+      };   
 
       try {
         const filter = this.cqlParser.read(Object.keys(labelMap)[0]);
